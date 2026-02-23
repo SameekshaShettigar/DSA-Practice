199476efee867d0f5babf66d2290d6a241998cc8
Sameeksha <164968851+SameekshaShettigar@users.noreply.github.com>
2026-02-23 20:20:02 +0530

    Rotate Array (LC 189)
    Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.

---
+++ Arrays/rotate_array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void reverse(vector<int>& arr,int start,int end){
+        while(start<end){
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+};
+
+
+tc:O(n)
+sc: O(1)
