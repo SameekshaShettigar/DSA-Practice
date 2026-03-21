4b3cc4775b425e09e008037ca5138a6603d62750
Sameeksha <164968851+SameekshaShettigar@users.noreply.github.com>
2026-03-21 08:35:28 +0530

    coin change solution using dynamic programming
    You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
    
    Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
    
    You may assume that you have an infinite number of each kind of coin.

---
+++ DP/coin_change.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount< 1) return 0;
+        vector<int> dp(amount+1);
+        for(int i = 1 ; i <= amount ; i++){
+            dp[i] = INT_MAX;
+            for(int j=0 ; j<coins.size() ; j++){
+                int coin = coins[j];
+                if((i-coin)>=0 && dp[i-coin] != INT_MAX){
+                    dp[i] = min(dp[i],1+dp[i-coin]);
+                }
+            }
+        }
+        if(dp[amount] == INT_MAX) return -1;
+        return dp[amount];
+    }
+       
+};
+
+TC:O(n*m)
+SC: O(n)
